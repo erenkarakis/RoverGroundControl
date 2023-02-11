0ed72df0f47a06a4cd5909a946c49aad61acbf25
Taha Eren Karakis <jpantherr@gmail.com>
2023-02-11 14:25:51 +0300

    Secret key deleted from file

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-VERSION = "0.1.0"
-SECRET_KEY = b"skljf%90ghnkjmg*009hp7eo3u&&apij7nk?p45jhap23ojep"
+import sk
+
+VERSION = "0.1.1"
+SECRET_KEY = sk.SECRET_KEY
 SESSION_COOKIE_NAME = "control_session"
---
+++ sk.py
@@ -0,0 +1,1 @@
+SECRET_KEY = b"skljf%90ghnkjmg*009hp7eo3u&&apij7nk?p45jhap23ojep"
